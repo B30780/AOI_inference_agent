3bbf3c946fdcfa0e81cb16622a6292c5a9fd9bab
B30780 <B30780@itri.org.tw>
2026-02-25 10:58:43 +0800

    Issue #2: Initialize all package directories (api, services, utils, models)

---
+++ app/api/__init__.py
@@ -0,0 +1,12 @@
+"""
+API routes package
+Contains all API endpoint definitions
+"""
+
+from fastapi import APIRouter
+
+# Create main API router
+api_router = APIRouter()
+
+# Import routers from other modules will be added here
+# from app.api import images, inference, results
